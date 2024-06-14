e5e1f8ce71d35d9f696cf0830da4de45f033fec1
ankita-singh27 <rajputanki5@gmail.com>
2024-06-15 00:26:35 +0530

    adding image size reducer

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -38,9 +38,13 @@ function isFileTypeSupported(type, supportedTypes) {
     return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder,quality) {
     const options = {folder};
     console.log("temp file path", file.tempFilePath);
+
+    if(quality) {
+        options.quality = quality;
+    }
     options.resource_type= "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
@@ -136,6 +140,56 @@ exports.videoUpload = async (req,res) => {
             message:'Video Successfully Uploaded',
         })
 
+    }
+    catch(error) {
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong',
+        })
+    }
+}
+
+//imageSizeReducer
+
+exports.imageSizeReducer = async (req,res) => {
+    try{
+        //data fetch
+        const { name, tags, email} = req.body;
+        console.log(name,tags,email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        //Validation
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("File Type:", fileType);
+
+        if(!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success:false,
+                message:'File format not supported',
+            })
+        }
+
+        //file format supported 
+        const response = await uploadFileToCloudinary(file, "sample", 30);
+        console.log(response);
+
+        //save entry in db
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url,
+        });
+
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:'Image Successfully Uploaded',
+        })
     }
     catch(error) {
         console.error(error);
